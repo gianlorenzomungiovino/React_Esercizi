cbd27742b5b6d8a9ebf5d2f93a53172aa47bdaa5
Gianlorenzo Mungiovino <gianlorenzo.mungiovino@gmail.com>
2025-01-07 16:23:25 +0100

    Update App.jsx

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Welcome } from "./componenti/welcome";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Welcome name="John" age={16} />
+    </>
+  );
 }
 
 export default App;
