ce94ad749a52ecd4d4e9f3e7a1de4f53e956c010
Gianlorenzo Mungiovino <gianlorenzo.mungiovino@gmail.com>
2025-01-07 16:35:05 +0100

    Update App.jsx

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -1,7 +1,20 @@
+// Create an AlertClock function component that renders a button.
+// When the button is clicked, an alert should be shown with the current time.
+// Render the AlertClock component within the App component. Make it so that the event handler is received as a prop by the AlertClock component, instead of being implemented within the component itself.
+
 import "./App.css";
+import { AlertClock } from "./componenti/AlertClock";
 
 function App() {
-  return <></>;
+  const handleShowTime = () => {
+    const currentTime = new Date().toLocaleTimeString();
+    alert(`Current Time: ${currentTime}`);
+  };
+  return (
+    <>
+      <AlertClock handleClick={handleShowTime} />
+    </>
+  );
 }
 
 export default App;
